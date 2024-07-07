e67bd73ac4ca3f63f33a11de7fa4da7daf0db380
azizulever <contact.azizulislam@gmail.com>
2024-07-07 20:40:13 +0600

    practiced python

--- practice.py
+++ practice.py
@@ -7,3 +7,55 @@
 # for i in marks: 
 #     added.append(i+2)
 # print(added)
+
+# x = 10
+# y = 3.14
+# name = "Alice"
+# is_active = True
+
+# if x > 5:
+#     print("x is greater than 5")
+# elif x == 5:
+#     print("x is 5")
+# else:
+#     print("x is less than 5")
+
+# for i in range(5):
+#     print(i)
+
+# while x > 0:
+#     print(x)
+#     x -= 1
+
+# def add(a, b):
+#     return a + b
+
+# result = add(5, 3)
+# print(result)
+
+# numbers = [1, 2, 3, 4, 5]
+# numbers.append(6)
+# print(numbers[2])
+
+# point = (10, 20)
+# print(point[0])
+
+# person = {"name": "Alice", "age": 25}
+# print(person["name"])
+# person["age"] = 26
+
+# unique_numbers = {1, 2, 3, 4, 5}
+# unique_numbers.add(6)
+# print(unique_numbers)
+
+
+# greeting = "Hello"
+# name = "Alice"
+# message = greeting + ", " + name + "!"
+# print(message)
+
+# user_input = input("Enter your name: ")
+# print("Hello, " + user_input)
+
+# squares = [x * x for x in range(10)]
+# print(squares)
