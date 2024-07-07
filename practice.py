9db2c1533635ed1f28e06f41a808a2beefb058d7
azizulever <contact.azizulislam@gmail.com>
2024-07-07 20:35:40 +0600

    practiced Python

--- practice.py
+++ practice.py
@@ -1,1 +1,9 @@
-print("Hello")
+# marks = [20, 30, 40, 50]
+# added = [value+2 for value in marks]
+# print(added)
+
+# marks = [20, 30, 40, 50]
+# added = []
+# for i in marks: 
+#     added.append(i+2)
+# print(added)
