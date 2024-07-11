c309662f0da7dee42aeae87d5a39ac02cac45854
azizulever <contact.azizulislam@gmail.com>
2024-07-11 22:33:49 +0600

    recursion practiced

---
+++ recursion.py
@@ -0,0 +1,22 @@
+# calculating factorial
+# def factorial(n):
+#     if(n==0 or n==1):
+#         return 1
+#     else:
+#         return n * factorial(n-1)
+
+# n = int(input("enter number "))
+# print(factorial(n))
+
+
+# fibonacci series
+def fibonacci(n):
+    if(n==0):
+        return 0
+    elif(n==1):
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+n = int(input("enter number "))
+print(fibonacci(n))
