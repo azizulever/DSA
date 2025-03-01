843369ee8b0d230c12a66fd715b224b85b6e490f
Azizul Islam <contact.azizulislam@gmail.com>
2025-03-01 09:34:53 +0600

    square pattern modified

--- patterns/square_pattern.cpp
+++ patterns/square_pattern.cpp
@@ -3,10 +3,11 @@ using namespace std;
 
 int main() {
     int n; cin >> n;
-    
+    int k = 1;
     for(int i = 1; i <= n; i++) {
         for(int j = 1; j <= n; j++) {
-            cout << j << ' ';
+            cout << k << ' ';
+            k++;
         }
         cout << '\n';
     }
